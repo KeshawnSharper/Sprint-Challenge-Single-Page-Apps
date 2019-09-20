d260301d5a178c2d6fb4190db26d2f7eb907e08b
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2019-09-20 13:41:32 -0400

    LocationList

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,23 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import LocationCard from "./LocationCard"
 
-export default function LocationsList() {}
+
+export default function LocationsList() {
+    const [locations, setLocations] = useState([])
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  Axios.get("https://rickandmortyapi.com/api/location/"
+  )
+  .then(res => setLocations(res.data.results))
+  .catch(error => console.log(error))
+  
+  }, []);
+console.log(locations)
+  return (
+    <section className="location-list grid-view">
+      {locations.map(location=> <LocationCard key={location.id} location={location} /> )}
+    </section>
+  );
+}
