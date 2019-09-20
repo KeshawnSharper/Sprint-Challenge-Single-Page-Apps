ae92e6667ecbd870a8ef7924f948388e7a7cff4a
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2019-09-20 11:45:36 -0400

    Routed all my pages and created links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
+import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList"
+import { Route } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage'
+import LocationsList from "./components/LocationsList"
+import EpisodeList from "./components/EpisodeList"
+import SearchForm from "./components/SearchForm.js";
 
 export default function App() {
+
+  const [name,setName] = useState([{}])
   return (
+   
+
     <main>
       <Header />
       
+      <TabNav /> 
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
+      <Route path="/locations" component={LocationsList} />
+      <Route path="/episodes" component={EpisodeList} />
+      <Route path="/search" component={SearchForm} />
+
     </main>
+   
+   
   );
 }
