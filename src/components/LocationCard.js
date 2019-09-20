5847b09741c70d6454556410aa04de04d0b57d7c
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2019-09-20 13:32:44 -0400

    LocationCard

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+
+export default function LocationCard(props) {
+
+  
+  return (<div className="grid-view">
+    <span>{props.location.name}</span>
+    <span>{props.location.type}</span>
+    <span>{props.location.dimension}</span>
+    <button>{props.location.residents.length}</button>
+    
+  </div>)
 }
---
+++ src/components/TabNav.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+
+// TODO: Add missing menu/tabs/nav below
+
+// Review Semantic UI Component options for nav-like UI:
+// https://react.semantic-ui.com/collections/menu/
+// https://react.semantic-ui.com/modules/tab/
+// https://react.semantic-ui.com/elements/button/
+// https://react.semantic-ui.com/collections/breadcrumb/
+
+
+//<NavLink to="/character">Characters</NavLink> 
+    
+export default function TabNav() {
+   
+   return (
+     <nav>
+       <NavLink to="/">Home</NavLink>
+       <NavLink to="/characters">Characters</NavLink> 
+       <NavLink to="/locations">Locations</NavLink>
+       <NavLink to="/episodes">Episodes</NavLink>
+       <NavLink to="/search">Search</NavLink>
+     </nav>
+   )
+
+      
+      
+      
+};
